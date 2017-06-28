41bfb42bbb444de9ccdd7a72968912c241227f09
Seyong Park <marching0531@naver.com>
2017-06-28 20:15:52 +0900

    Add Description to Block.h

--- Block.h
+++ Block.h
@@ -14,24 +14,48 @@ public:
     int rotateCount;
     int myColor;
 
+    //Description : Default constructor of block object
+    //Parameter : None
     Block();
 
+    //Description : Parameter added constructor of block object 
+    //Parameter : window to create block, block id, row and column to indicate
+    //block's location
     Block(WINDOW* win, int id, int row, int col);
 
+    //Description : To detect collision, Copy current games grid to
+    //shared virtual grid
+    //Parameter : A two-dimensional integer array to copy
     void sendVA(int virtualArray[20][20]);
 
+    //Description : Collision check method to check if block spawn space
+    //is collided with other block
+    //Parameter : Window to check collision, and row, column indicating block's location
     bool isCollide(WINDOW* win, int row, int col);
 
+    //Description : Rotate Current Block
+    //Parameter : Window to rotate block and row,column indicating block's location
     void rotate(WINDOW* win, int row, int col);
 
+
+    //Description : Move down Current Block
+    //Parameter : Window to move down block and row,column indicating block's location
     bool down(WINDOW* win, int row, int col);
 
+    //Description : Move right Current Block
+    //Parameter : Window to move down block and row,column indicating block's location
     bool right(WINDOW* win, int row, int col);
 
+    //Description : Move left Current Block
+    //Parameter : Window to move left block and row,column indicating block's location
     bool left(WINDOW* win, int row, int col);
 
+    //Description : Print right color to current block
+    //Parameter : Window to print block and color to paint block, and row,column indicating block's location
     void printBlock(WINDOW* win, int color, int row, int col);
 
+    //Description : Bring Predefined each block's template
+    //Parameter : None
     void bringTemplates();
 };
 #endif
